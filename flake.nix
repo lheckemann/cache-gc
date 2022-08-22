10ec93fabc4d89eb41bebf64ec2ac0c0cb9ce80e
Linus Heckemann <git@sphalerite.org>
2022-08-22 11:58:32 +0200

    Refactor flake
    - Support multiple systems (currently x86_64-linux and aarch64-linux)
    - Move package expression out into its own flake
    - Don't rely on overlay, in particular do not force it in the NixOS module
    - Add rudimentary version info to package
    - Use nixos-22.05 rather than release-22.05 for better cache coverage
    - Fix some deprecated output names
    - Add hydra jobs

--- flake.nix
+++ flake.nix
@@ -1,45 +1,34 @@
 {
   description = "Nix binary cache garbage collector";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/release-22.05;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
 
-  outputs = { self, nixpkgs }: {
-    overlay = final: prev: {
-      cache-gc =
-        let
-          nix = final.nixVersions.nix_2_9;
-          inherit (final) rustPlatform lib coreutils jq findutils;
-        in rustPlatform.buildRustPackage {
-          src = lib.cleanSource ./.;
-          name = "cache-gc";
-          cargoLock.lockFile = ./Cargo.lock;
-          buildInputs = [ final.bash ];
-          postInstall = ''
-            mkdir -p $out/libexec/cache-gc
-            mv $out/bin/gc $out/libexec/cache-gc/
-            cp $src/add-registration-times.jq $out/libexec/cache-gc
-            install -m0755 $src/run.sh $out/bin/cache-gc
-            sed -i $out/bin/cache-gc \
-              -e "2iexport PATH=$out/libexec/cache-gc:${lib.makeBinPath [ coreutils jq nix findutils ]}" \
-              -e "2ilibexec_dir=$out/libexec/cache-gc"
-            patchShebangs $out/bin/cache-gc
-          '';
-        };
+  outputs = { self, nixpkgs }: let
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"];
+  in {
+    overlays.default = final: prev: {
+      cache-gc = prev.callPackage ./package.nix { nix = final.nixVersions.nix_2_9; src = self; };
     };
 
-    defaultPackage.x86_64-linux = self.packages.cache-gc.x86_64-linux;
-    packages.cache-gc.x86_64-linux =
-      let
-        pkgs = import nixpkgs { overlays = [ self.overlay ]; system = "x86_64-linux"; };
-      in pkgs.cache-gc;
+    defaultPackage = forAllSystems (system: self.packages.${system}.cache-gc);
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      cache-gc = pkgs.callPackage ./package.nix { nix = pkgs.nixVersions.nix_2_9; src = self; };
+      default = cache-gc;
+    });
 
-    devShell.x86_64-linux =
+    hydraJobs = self.outputs.packages;
+
+    devShells = forAllSystems (system:
       let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in pkgs.mkShell {
-        name = "cache-gc-dev";
-        buildInputs = with pkgs; [ cargo rustfmt rustc ];
-      };
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          name = "cache-gc-dev";
+          buildInputs = with pkgs; [ cargo rustfmt rustc ];
+        };
+      });
 
     nixosModules.cache-gc = { config, lib, pkgs, ... }: with lib;
       let
@@ -53,6 +42,13 @@
             description = ''
               Path containing the binary cache to clean up.
             '';
+          };
+          package = mkOption {
+            type = types.package;
+            description = ''
+              The cache-gc package used for cleaning the cache.
+            '';
+            default = self.outputs.packages.${pkgs.system}.cache-gc;
           };
           days = mkOption {
             default = 90;
@@ -85,13 +81,12 @@
           };
         };
         config = mkIf cfg.enable {
-          nixpkgs.overlays = [ self.overlay ];
           systemd.services.cache-gc = {
             description = "Nix binary cache garbage collector";
             environment.HOME = "/run/cache-gc";
             serviceConfig = {
               CapabilityBoundingSet = [ "" ];
-              ExecStart = "${pkgs.cache-gc}/bin/cache-gc --days ${toString cfg.days} --delete ${cfg.path}";
+              ExecStart = "${cfg.package}/bin/cache-gc --days ${toString cfg.days} --delete ${cfg.path}";
               LockPersonality = true;
               MemoryDenyWriteExecute = true;
               NoNewPrivileges = true;
